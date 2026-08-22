bc1d0bf2767bc2053cdc45d210b317cfb2cfb8c3
4C01 t <Catcat-jia@foxmail.com>
2026-08-22 23:10:26 +0800

    修复该死的split

---
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/PlayerGeneratorCollectListener.java
@@ -0,0 +1,66 @@
+package com.andrei1058.bedwars.listeners;
+
+import com.andrei1058.bedwars.BedWars;
+import com.andrei1058.bedwars.api.configuration.ConfigPath;
+import com.andrei1058.bedwars.api.events.player.PlayerGeneratorCollectEvent;
+import com.andrei1058.bedwars.arena.Arena;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import static com.andrei1058.bedwars.BedWars.config;
+import static com.andrei1058.bedwars.BedWars.plugin;
+
+public class PlayerGeneratorCollectListener implements Listener {
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onCollect(PlayerGeneratorCollectEvent event) {
+        Item item = event.getItem();
+        Material material = item.getItemStack().getType();
+        Player player = event.getPlayer();
+        if (item.getItemStack().hasItemMeta()) {
+            //noinspection ConstantConditions
+            if (item.getItemStack().getItemMeta().hasDisplayName()) {
+                if (item.getItemStack().getItemMeta().getDisplayName().contains("custom") && plugin.getConfig().getBoolean(ConfigPath.GENERAL_CONFIGURATION_ENABLE_GEN_SPLIT)) {
+                    ItemMeta itemMeta = new ItemStack(material).getItemMeta();
+                    item.getItemStack().setItemMeta(itemMeta);
+                    Location location = event.getPlayer().getLocation();
+                    for (Entity entity : player.getWorld().getNearbyEntities(location, 2.0D, 2.0D, 2.0D)){
+                        if (entity instanceof Player && entity != player){
+                            Player nearby = (Player) entity;
+                            if (Arena.getArenaByPlayer(player).getConfig().getBoolean("xp")) {
+                                int xps = 0;
+                                switch (material) {
+                                    case IRON_INGOT:
+                                        xps = event.getItem().getItemStack().getAmount() * config.getInt(ConfigPath.CURRENCY_IRON_PRICE);
+                                        break;
+                                    case GOLD_INGOT:
+                                        xps = event.getItem().getItemStack().getAmount() * config.getInt(ConfigPath.CURRENCY_GOLD_PRICE);
+                                        break;
+                                    case EMERALD:
+                                        xps = event.getItem().getItemStack().getAmount() * config.getInt(ConfigPath.CURRENCY_EMERALD_PRICE);
+                                        break;
+                                }
+                                if(xps != 0) {
+                                    nearby.giveExpLevels(xps);
+                                    nearby.playSound(nearby.getLocation(), Sound.valueOf(BedWars.getForCurrentVersion("SUCCESSFUL_HIT", "ENTITY_EXPERIENCE_ORB_PICKUP", "ENTITY_EXPERIENCE_ORB_PICKUP")), 0.6f, 1.3f);
+                                }
+                            }
+                            else {
+                                nearby.getInventory().addItem(item.getItemStack());
+                                nearby.playSound(player.getLocation(), Sound.valueOf(BedWars.getForCurrentVersion("ITEM_PICKUP", "ENTITY_ITEM_PICKUP", "ENTITY_ITEM_PICKUP")), 0.6f, 1.3f);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/PlayerPickUpListener.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/PlayerPickUpListener.java
@@ -6,15 +6,11 @@ import com.andrei1058.bedwars.arena.Arena;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerPickupItemEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import static com.andrei1058.bedwars.BedWars.*;
 
@@ -22,48 +18,11 @@ public class PlayerPickUpListener implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
     public void onPickUp(PlayerPickupItemEvent event){
         Player player = event.getPlayer();
-        Item item = event.getItem();
         Material material = event.getItem().getItemStack().getType();
         if(Arena.getArenaByPlayer(player) == null){
             return;
         }
         int xps = 0;
-        if (item.getItemStack().hasItemMeta()) {
-            //noinspection ConstantConditions
-            if (item.getItemStack().getItemMeta().hasDisplayName()) {
-                if (item.getItemStack().getItemMeta().getDisplayName().contains("custom") && plugin.getConfig().getBoolean(ConfigPath.GENERAL_CONFIGURATION_ENABLE_GEN_SPLIT)) {
-                    ItemMeta itemMeta = new ItemStack(material).getItemMeta();
-                    item.getItemStack().setItemMeta(itemMeta);
-                    Location location = event.getPlayer().getLocation();
-                    for (Entity entity : player.getWorld().getNearbyEntities(location, 2.0D, 2.0D, 2.0D)){
-                        if (entity instanceof Player && entity != player){
-                            Player nearby = (Player) entity;
-                            if (Arena.getArenaByPlayer(player).getConfig().getBoolean("xp")) {
-                                switch (material) {
-                                    case IRON_INGOT:
-                                        xps = event.getItem().getItemStack().getAmount() * config.getInt(ConfigPath.CURRENCY_IRON_PRICE);
-                                        break;
-                                    case GOLD_INGOT:
-                                        xps = event.getItem().getItemStack().getAmount() * config.getInt(ConfigPath.CURRENCY_GOLD_PRICE);
-                                        break;
-                                    case EMERALD:
-                                        xps = event.getItem().getItemStack().getAmount() * config.getInt(ConfigPath.CURRENCY_EMERALD_PRICE);
-                                        break;
-                                }
-                                if(xps != 0) {
-                                    nearby.giveExpLevels(xps);
-                                    nearby.playSound(nearby.getLocation(), Sound.valueOf(BedWars.getForCurrentVersion("SUCCESSFUL_HIT", "ENTITY_EXPERIENCE_ORB_PICKUP", "ENTITY_EXPERIENCE_ORB_PICKUP")), 0.6f, 1.3f);
-                                }
-                            }
-                            else {
-                                nearby.getInventory().addItem(item.getItemStack());
-                                nearby.playSound(player.getLocation(), Sound.valueOf(BedWars.getForCurrentVersion("ITEM_PICKUP", "ENTITY_ITEM_PICKUP", "ENTITY_ITEM_PICKUP")), 0.6f, 1.3f);
-                            }
-                        }
-                    }
-                }
-            }
-        }
         if (Arena.getArenaByPlayer(player).getConfig().getBoolean("xp")) {
             if (nms.materialExperienceBottle().equals(material)){
                 xps = event.getItem().getItemStack().getAmount() * 10;
